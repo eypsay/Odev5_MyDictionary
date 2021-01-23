ed073f1a8a7f9760b9103a55a08af1721eab6af0
eyp <eypsay@yahoo.com>
2021-01-23 17:50:08 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> courses = new MyDictionary<int, string>();
+
+            courses.Add(101, "Introducton to Computer");
+            courses.Add(201, "Java");
+            courses.Add(202, "OOP");
+
+            Console.WriteLine(courses.Len);
+            foreach (var item in courses.Keys)
+            {
+                Console.WriteLine("Keys:"+item);
+            }
+            foreach (var item in courses.Values)
+            {
+                Console.WriteLine("Values:" + item);
+            }
+        }
+    }
+}
